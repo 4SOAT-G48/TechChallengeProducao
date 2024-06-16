0e3e370d28a3016ec07e9fe15f0bf8ea37e370d4
André Felipe Laus <andre.felipe.laus@gmail.com>
2024-06-16 18:33:24 -0300

    Commit inicial

---
+++ src/main/java/br/com/fiap/soat/grupo48/grupo48/producao/Grupo48ProducaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.soat.grupo48.grupo48.producao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo48ProducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo48ProducaoApplication.class, args);
+	}
+
+}
